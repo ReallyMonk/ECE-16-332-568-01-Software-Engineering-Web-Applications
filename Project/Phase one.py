2fe6f05aa961e9921244dc45bd5d1b4efd6986b2
ReallyMonk <hourunlinxa@gmail.com>
2020-02-28 13:31:36 -0500

    'p1_final'

--- Project/Phase one.py
+++ Project/Phase one.py
@@ -1,7 +1,9 @@
 import requests
 import json
-#import time
+from datetime import datetime
 import pymongo
+import time
+
 '''
 url = "https://query1.finance.yahoo.com/v8/finance/chart/GOOG?symbol=GOOG&period1=1441166400&period2=1519621200&interval=1d&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=RGKkY5jfStA&corsDomain=finance.yahoo.com"
 url = "https://query1.finance.yahoo.com/v8/finance/chart/SNE?symbol=SNE&period1=1582587792&period2=1582760592&interval=1m&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=RGKkY5jfStA&corsDomain=finance.yahoo.com"
@@ -12,7 +14,7 @@ path = 'D:\Rutgers\\2nd Semester\SOFTWR ENGG WEB APPL\Homework\Project\data\\'
 '''
 
 
-def get_year_data():
+def get_year_data(t_start, t_end):
     print('connect to MongoDB')
     # connect to MongoDB
     client = pymongo.MongoClient(host='localhost', port=27017)
@@ -25,7 +27,7 @@ def get_year_data():
     ]
     for name in companies:
         print('Download ', name)
-        url = 'https://query1.finance.yahoo.com/v8/finance/chart/' + name + '?symbol=' + name + '&period1=1441166400&period2=1519621200&interval=1d&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=RGKkY5jfStA&corsDomain=finance.yahoo.com'
+        url = 'https://query1.finance.yahoo.com/v8/finance/chart/' + name + '?symbol=' + name + '&period1=' + t_start + '&period2=' + t_end + '&interval=1d&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=RGKkY5jfStA&corsDomain=finance.yahoo.com'
 
         request = requests.get(url)
         print('Write ', name)
@@ -47,4 +49,11 @@ def get_year_data():
         collection.insert_one(all_info)
 
 
-get_year_data()
+# please set the date you want here as the format yy-mm-dd
+start_t = '2015-04-05'
+end_t = '2016-04-08'
+
+strt_stp = str(int(time.mktime(time.strptime(start_t+' 09:30:00', '%Y-%m-%d %H:%M:%S'))))
+endt_stp = str(int(time.mktime(time.strptime(end_t+' 16:00:00', '%Y-%m-%d %H:%M:%S'))))
+
+get_year_data(strt_stp, endt_stp)
